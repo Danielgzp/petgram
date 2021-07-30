64aad2cb9e0a2100998bde591d608e496843ca3e
Daniel Gonzalez <danielgonzalez20020110@gmail.com>
2021-07-29 22:34:17 -0400

    Clase 15

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Category } from "../Category";
 import { List, Item } from "./styles";
-import { categories } from "../../../api/db.json";
 
 export const ListOfCategories = () => {
+    const [categories, setCategories] = useState([]);
+    const [state, setState] = useState({
+        loading: false,
+        error: null,
+    });
+
+    useEffect(async function () {
+        setState({ loading: true, error: null });
+        try {
+            const response = await fetch(
+                "https://petgram-danielgzp.vercel.app/categories"
+            );
+            const data = await response.json();
+            setCategories(data);
+            setState({ loading: false, error: null });
+        } catch (error) {
+            console.log(error);
+            setState({ loading: false, error: error });
+        }
+    }, []);
+
+    if (state.loading) {
+        <h2>cargando</h2>;
+    }
+    if (state.error) {
+        <h2>Ocurrio un errorsito 😎</h2>;
+    }
+
     return (
         <List>
-            {categories.map(category => (
+            {categories.map((category) => (
                 <Item key={category.id}>
                     <Category {...category} />
                     {/*Escribir Spread Operator {...category} trae todos los atributos, esto puede sustutuir la manera manual de ahcerlo asi: cover={category.cover} emoji={category.emoji}*/}
