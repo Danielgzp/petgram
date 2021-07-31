bdd0881956fa1b0c76bfb29a02f22ab7db3e356b
Daniel Gonzalez <danielgonzalez20020110@gmail.com>
2021-07-31 14:03:16 -0400

    Apollo y GraphQl funcionando

--- src/components/ListOfPhotoCards/index.jsx
+++ src/components/ListOfPhotoCards/index.jsx
@@ -1,13 +1,38 @@
+//DEPENDENCIES
 import React from "react";
+import { useQuery, gql} from "@apollo/client";
 
+
+//COMPONENTS
 import { PhotoCard } from "../PhotoCard";
-import { photos } from '../../../api/db.json'
+
+const whitPhotos = gql`
+    query getPhotos {
+        photos {
+            id
+            categoryId
+            src
+            likes
+            userId
+            liked
+        }
+    }
+`;
 
 export const ListOfPhotoCards = () => {
+    const { loading, error, data } = useQuery(whitPhotos);
+
+    if (error) {
+        return <h2>Internal Server Error</h2>;
+    }
+    if (loading) {
+        return <h2>Loading...</h2>;
+    }
+
     return (
         <ul>
-            {photos.map((photo) => (
-                <PhotoCard key={photo.id} {...photo} id={photo.id} />
+            {data.photos.map((photo) => (
+                <PhotoCard key={photo.id} {...photo} />
             ))}
             {/* {[1, 2, 3, 4, 5, 6, 7].map((id) => (
                 <PhotoCard key={id} id={id}  />
--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { App } from "./routes/App";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+//APP
+import { App } from "./routes/App.jsx";
+
+const client = new ApolloClient({
+  uri: "https://petgram-danielgzp.vercel.app/graphql",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+
+  document.getElementById("app")
+);
