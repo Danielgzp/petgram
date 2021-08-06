d9618cc812f3bd7f1ff28a4d95fe9240efa0ffe8
Daniel Gonzalez <danielgonzalez20020110@gmail.com>
2021-08-06 14:34:21 -0400

    Clase 38

--- src/pages/NotRegisteredUser.jsx
+++ src/pages/NotRegisteredUser.jsx
@@ -3,10 +3,12 @@ import React, { useState } from "react";
 import { useAuthContext } from "../hooks/Context";
 import { UserForm } from "../components/UserForm";
 import { useRegisterMutation } from "../containers/useRegisterMutation";
+import { useLoginMutation } from "../containers/useLoginMutation";
 
 export const NotRegisteredUser = () => {
     const { activateUser } = useAuthContext();
     const { registerMutation } = useRegisterMutation();
+    const { loginMutation } = useLoginMutation();
     const [ state, setState] = useState({
         loading: false, 
         error: null
@@ -14,18 +16,33 @@ export const NotRegisteredUser = () => {
 
     const errorMsg =
         state.error && "El usuario ya existe o hay algún problema.";
+        const errorMsgLogin =
+            state.error && "La contraseña es incorrecta o el usuario no existe.";
 
     const onSubmit = async ({ email, password }) => {
         const input = { email, password };
         const variables = { input };
         setState({loading: true, error: null})
         try{
-            await registerMutation({ variables }).then(activateUser);
+            await registerMutation({ variables });
             setState({loading: false, error: null})
         }catch(error){
             setState({loading: false, error: error})
         }
     };
+
+    const onLogin = async ({ email, password }) => {
+        const input = { email, password };
+        const variables = { input };
+        setState({ loading: true, error: null });
+        try {
+            await loginMutation({ variables }).then(activateUser);
+            setState({ loading: false, error: null });
+        } catch (error) {
+            setState({ loading: false, error: error });
+        }
+    };
+
 
     return (
         <>
@@ -37,8 +54,9 @@ export const NotRegisteredUser = () => {
             />
             <UserForm
                 disabled={state.loading}
-                onSubmit={activateUser}
+                onSubmit={onLogin}
                 title="Iniciar Sesion"
+                error={errorMsgLogin}
             />
         </>
 
